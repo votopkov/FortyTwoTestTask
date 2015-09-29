6ee3b1bd7a721d46f936187661d13a1465d8b254
_smith_ <young_smith@mail.ru>
2015-09-29 09:46:31 +0300

    ticket: ticket_1

--- task/tests.py
+++ task/tests.py
@@ -2,10 +2,7 @@
 from django.core.urlresolvers import reverse
 from django.test import Client
 from django.test import TestCase
-from django.test import RequestFactory
 from models import Profile
-from models import Requests
-from http_request import SaveHttpRequestMiddleware
 client = Client()
 
 
@@ -15,87 +12,59 @@ class ProfileMethodTests(TestCase):
         Profile.objects.create(name=u"Владимир", last_name=u"Отопков",
                                contacts=u"+380937080855")
         Profile.objects.create(name=u"Василий", last_name=u"Петров")
+        # get main page
+        self.response = self.client.get(reverse('task:index'))
 
     def test_enter_main_page(self):
         """
-        Testing my profile shown on the main page
-        Testing one profile if two
-        Testing unicode
+        Test entering main page
         """
-        response = self.client.get(reverse('task:index'))
         # if index page exists
-        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_profile(self):
+        """
+        Testing profile shown in the page
+        """
         # get profile
-        self.profile = Profile.objects.first()
-        self.assertEqual(response.context['profile'], self.profile)
-        # test if not another profile on index
-        self.assertNotEqual(response.context['profile'],
-                            Profile.objects.get(id=2))
+        profile = Profile.objects.first()
+        self.assertEqual(self.response.context['profile'], profile)
         # test profile data exist on the main page
-        self.assertContains(response, u'Отопков')
-        self.assertContains(response, u'Владимир')
-        self.assertContains(response, '+380937080855')
-        # test if not another profile on the main page
-        self.assertNotIn('Василий', response.content)
+        self.assertContains(self.response, u'Отопков')
+        self.assertContains(self.response, u'Владимир')
+        self.assertContains(self.response, '+380937080855')
+
+    def test_non_another_profile(self):
+        """
+        Test if exist another profile in the page
+        """
+        # test if not another profile on index
+        self.assertNotEqual(self.response.context['profile'],
+                            Profile.objects.get(id=2))
+        self.assertNotIn('Василий', self.response.content)
 
 
-class SaveHttpRequestTests(TestCase):
+class ProfileNoDataMethodTests(TestCase):
 
     def setUp(self):
-        self.save_http = SaveHttpRequestMiddleware()
-        self.new_request = RequestFactory()
-        Requests.objects.create(request='request_1')
-        Requests.objects.create(request='request_2')
+        # get main page
+        self.response = self.client.get(reverse('task:index'))
 
-    def test_request_list(self):
+    def test_enter_main_page(self):
         """
-        Testing request list view function
+        Test entering main page
         """
-        # get requests
-        response = client.get(reverse('task:request_list'),
-                              content_type='application/json',
-                              HTTP_X_REQUESTED_WITH='XMLHttpRequest')
-        # get first request
-        request = Requests.objects.get(request='request_1')
-        # get second request
-        request_2 = Requests.objects.get(request='request_2')
-        # get nonexistent request
-        request_3 = Requests.objects.filter(request='request_3')
-        # test getting request list
-        self.assertEquals(response.status_code, 200)
-        # test first request in response content
-        self.assertContains(response, request)
-        # test second request in response content
-        self.assertContains(response, request_2)
-        # test not exist request in response content
-        self.assertNotIn(response.content, request_3)
+        # if index page exists
+        self.assertEqual(self.response.status_code, 200)
 
-    def test_request_detail(self):
+    def test_profile(self):
         """
-        Testing request detail view function
+        Testing profile shown in the page
         """
-        # get first request
-        request = Requests.objects.get(request='request_1')
-        # get first request
-        response = client.get(reverse('task:request_detail',
-                                      args=(request.id, )))
-        # get nonexistent request
-        response_2 = client.get(reverse('task:request_detail',
-                                        args=(555, )))
-        # test gettings page with first profile
-        self.assertEqual(response.status_code, 200)
-        # test gettings page with nonexistent request
-        self.assertEqual(response_2.status_code, 404)
-        # test first request data on the page
-        self.assertIn(request.request, response.content)
-        # test context of the detail request
-        self.assertEqual(request, response.context['obj'])
-
-    def test_save_request(self):
-        """
-        Test SaveHttpRequestMiddleware()
-        """
-        # save request to DB
-        self.save_http.process_request(request=self.new_request)
-        # test saving request to DB
-        self.assertEqual(Requests.objects.all().count(), 3)
+        # get profile
+        profile = Profile.objects.all().count()
+        self.assertEqual(profile, 0)
+        # test profile data exist on the main page
+        self.assertNotContains(self.response, u'Отопков')
+        self.assertNotContains(self.response, u'Владимир')
+        self.assertNotContains(self.response, '+380937080855')
