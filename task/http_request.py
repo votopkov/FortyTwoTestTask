2e23a327255dafa65192dffbe073e17a35cbb6d6
_smith_ <young_smith@mail.ru>
2015-10-01 12:42:15 +0300

    merge master to t3

--- task/http_request.py
+++ task/http_request.py
@@ -8,5 +8,6 @@ class SaveHttpRequestMiddleware(object):
         if request.is_ajax():
             pass
         else:
-            save_request = Requests(request=request, path=request.build_absolute_uri())
+            save_request = Requests(request=request,
+                                    path=request.build_absolute_uri())
             return save_request.save()
