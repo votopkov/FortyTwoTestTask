959d55c59b43c1e90ed2e190b624f04c186f53b7
_smith_ <young_smith@mail.ru>
2015-10-09 13:57:26 +0300

    ticket:1 for #1 add fixture_dirs

--- testtask/settings.py
+++ testtask/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-FIXTURE_DIRS = (os.path.join(BASE_DIR, '/fixtures/'))
+
+FIXTURE_DIRS = (
+    os.path.join(BASE_DIR, 'fixtures/'),
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
